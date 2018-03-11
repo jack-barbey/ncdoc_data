1956d15d61703a72ff7c2865e2cf018debc3ef36
OpsWorks user jwalsh <jwalsh@dsapp-super-master.localdomain>
2018-03-11 23:25:44 +0000

    enable user to choose the directory where the files are stored

--- ncdoc_des2csv.sh
+++ ncdoc_des2csv.sh
@@ -6,29 +6,37 @@ set -e
 # raw files are fixed width and come with definition files.
 # convert those definition files into in2csv format then
 # run in2csv to save in csv format.
+# 
+# Inputs:
+#  1) directory to store raw and processed files
+#  2) the NCDOC file name
 
-ZIPFILE=$1
+
+ETL_DIRECTORY=$1
+
+ZIPFILE=$2
 FILE_NO_EXTENSION="${ZIPFILE%.zip}"
+
 URL="http://www.doc.state.nc.us/offenders"
 
 # download the file
 wget -N \
-     -P preprocessed/ \
+     -P "$ETL_DIRECTORY" \
      "$URL"/"$ZIPFILE"
 
 # unzip
 unzip -o \
-      -d preprocessed/ \
-      preprocessed/"$ZIPFILE"
+      -d "$ETL_DIRECTORY" \
+      "$ETL_DIRECTORY"/"$ZIPFILE"
 
 # create schema file
-echo 'column,start,length' > preprocessed/"$FILE_NO_EXTENSION"_schema.csv
-sed -E 's/[ ]{2,}/,/g' preprocessed/"$FILE_NO_EXTENSION".des | \
+echo 'column,start,length' > "$ETL_DIRECTORY"/"$FILE_NO_EXTENSION"_schema.csv
+sed -E 's/[ ]{2,}/,/g' "$ETL_DIRECTORY"/"$FILE_NO_EXTENSION".des | \
 grep -vE "^Name," | \
-cut -d',' -f1,4-5 >> preprocessed/"$FILE_NO_EXTENSION"_schema.csv
+cut -d',' -f1,4-5 >> "$ETL_DIRECTORY"/"$FILE_NO_EXTENSION"_schema.csv
 
 # do the conversion 
-in2csv -s preprocessed/"$FILE_NO_EXTENSION"_schema.csv \
-       preprocessed/"$FILE_NO_EXTENSION".dat | \
-tr -d '?' > preprocessed/"$FILE_NO_EXTENSION".csv
+in2csv -s "$ETL_DIRECTORY"/"$FILE_NO_EXTENSION"_schema.csv \
+       "$ETL_DIRECTORY"/"$FILE_NO_EXTENSION".dat | \
+tr -d '?' > "$ETL_DIRECTORY"/"$FILE_NO_EXTENSION".csv
 
--- ncdoc_parallel.sh
+++ ncdoc_parallel.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+if [[ $# -eq 0 ]]
+  then
+    echo "Please provide a directory for storing data files"
+    exit 1
+fi
+
+ETL_DIRECTORY=$1
+
 # create directory to hold outputs
-mkdir -p preprocessed
+mkdir -p "$ETL_DIRECTORY"
 
 # download and process ncdoc data in parallel
-parallel './ncdoc_des2csv.sh {}' ::: OFNT3AA1.zip APPT7AA1.zip APPT9BJ1.zip INMT4AA1.zip INMT4BB1.zip INMT4CA1.zip INMT9CF1.zip OFNT1BA1.zip OFNT3BB1.zip OFNT3CE1.zip OFNT3DE1.zip OFNT9BE1.zip
+parallel "./ncdoc_des2csv.sh $ETL_DIRECTORY {}" ::: OFNT3AA1.zip APPT7AA1.zip APPT9BJ1.zip INMT4AA1.zip INMT4BB1.zip INMT4CA1.zip INMT9CF1.zip OFNT1BA1.zip OFNT3BB1.zip OFNT3CE1.zip OFNT3DE1.zip OFNT9BE1.zip
